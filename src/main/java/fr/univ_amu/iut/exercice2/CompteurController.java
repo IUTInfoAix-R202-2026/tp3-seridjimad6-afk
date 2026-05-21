f9611273f9fc445d300824207940a86baaeb03e2
Imad SERIDJ <seridjimad6@gmail.com>
2026-05-21 13:57:15 +0000

    Exercice 2 terminé

--- src/main/java/fr/univ_amu/iut/exercice2/CompteurController.java
+++ src/main/java/fr/univ_amu/iut/exercice2/CompteurController.java
@@ -44,10 +44,14 @@ public class CompteurController {
   private void initialize() {
     // TODO exercice 2 : lier le texte du label à la valeur du compteur.
     //
-    // Utiliser Bindings.convert(...) ou compteur.asString() pour obtenir un StringBinding,
+    labelCompteur.textProperty().bind(compteur.asString());
+    // Utiliser Bindings.convert(...) ou compteur.asString() pour obtenir un
+    // StringBinding,
     // puis labelCompteur.textProperty().bind(...).
     //
-    // Ainsi, chaque fois que la valeur de `compteur` change (via incrementer/decrementer/reinit),
+
+    // Ainsi, chaque fois que la valeur de `compteur` change (via
+    // incrementer/decrementer/reinit),
     // le label se met à jour automatiquement - aucun setText() à appeler !
   }
 
@@ -59,18 +63,21 @@ public class CompteurController {
    */
   @FXML
   private void incrementer() {
+    compteur.set(compteur.get() + 1);
     // TODO exercice 2 : incrémenter la valeur de la propriété compteur.
   }
 
   /** Action du bouton −. Décrémente le compteur. */
   @FXML
   private void decrementer() {
+    compteur.set(compteur.get() - 1);
     // TODO exercice 2 : décrémenter la valeur de la propriété compteur.
   }
 
   /** Action du bouton Réinitialiser. Remet le compteur à zéro. */
   @FXML
   private void reinitialiser() {
+    compteur.set(0);
     // TODO exercice 2 : remettre la propriété compteur à 0.
   }
 
